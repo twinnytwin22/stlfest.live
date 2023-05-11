5a06f48dd07c73e589081a499aca0fe253e0c723
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-11 14:00:32 -0700

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,32 @@
-import Head from "next/head"
 import Providers from "../lib/providers"
-import HeroBanner from "../ui/hero/HeroBanner"
 import Footer from "../ui/navigation/Footer"
 import NavBar from "../ui/navigation/NavBar"
 export const metadata = {
   title: 'STL FEST 2023',
   description: 'STL FEST 2023',
+  openGraph: {
+    title: "RandalHerndon.com",
+    description:
+      "STL FEST 2023",
+    url: "https:/stlfest.live",
+    siteName: "STL FEST",
+    images: [
+      {
+        url: "https://i.imgur.com/o8CKJ0e.jpeg",
+        width: 800,
+        height: 800,
+      },
+      {
+        url: "https://i.imgur.com/o8CKJ0e.jpeg",
+        width: 1800,
+        height: 1800,
+        alt: "STLFEST",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+
+  },
 }
 
 export default function RootLayout({ children }) {
