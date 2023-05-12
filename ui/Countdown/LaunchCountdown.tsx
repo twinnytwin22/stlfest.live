0ac78c8364157d5d3797c66e845f4aaea9cf32f4
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-11 17:39:58 -0700

    finish font update

--- ui/Countdown/LaunchCountdown.tsx
+++ ui/Countdown/LaunchCountdown.tsx
@@ -41,23 +41,23 @@ const LaunchCountdown = ({ targetDate }: { targetDate: Date }) => {
       <div className="grid grid-cols-4 md:grid-cols-5 items-end text-white text-center justify-items-center content-center justify-center mx-auto font-[bubblegum-pop-highlight]">
         <div className="flex flex-col justify-items-center content-center justify-center bg-blue-700 p-2.5 w-24">
           <div className="text-2xl md:text-4xl font-bold">{timeRemaining.weeks % 100}</div>
-          <div className="text-xs font-semibold md:p-1 font-[owners]">weeks</div>
+          <div className="text-xs font-semibold md:p-1 font-[owners2]">weeks</div>
         </div>
         <div className="flex flex-col justify-items-center content-center justify-center bg-blue-700 p-2.5 w-24">
           <div className="text-2xl md:text-4xl font-bold">{timeRemaining.days}</div>
-          <div className="text-xs font-semibold md:p-1 font-[owners]">days</div>
+          <div className="text-xs font-semibold md:p-1 font-[owners2]">days</div>
         </div>
         <div className="flex flex-col justify-items-center content-center justify-center bg-blue-700 p-2.5 w-24">
           <div className="text-2xl md:text-4xl font-bold">{timeRemaining.hours}</div>
-          <div className="text-xs font-semibold md:p-1 font-[owners]">hours</div>
+          <div className="text-xs font-semibold md:p-1 font-[owners2]">hours</div>
         </div>
         <div className="flex flex-col justify-items-center content-center justify-center bg-blue-700 p-2.5 w-24">
           <div className="text-2xl md:text-4xl font-bold">{timeRemaining.minutes}</div>
-          <div className="text-xs font-semibold md:p-1 font-[owners]">minutes</div>
+          <div className="text-xs font-semibold md:p-1 font-[owners2]">minutes</div>
         </div>
         <div className="hidden md:flex flex-col justify-items-center content-center justify-center bg-blue-700 p-2.5 w-24">
           <div className="text-2xl md:text-4xl font-bold">{timeRemaining.seconds}</div>
-          <div className="text-xs font-semibold md:p-1 font-[owners]">seconds</div>
+          <div className="text-xs font-semibold md:p-1 font-[owners2]">seconds</div>
         </div>
       </div>
     </div>
--- ui/HomeHeader.tsx
+++ ui/HomeHeader.tsx
@@ -19,7 +19,7 @@ function HomeHeader() {
                 transition={{ delay: 0.2, duration: 0.5 }}>
                 <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                     <Link href="" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm  bg-blue-700 text-white  hover:bg-blue-900 hover:scale-105 duration-300 ease-in-out" role="alert">
-                        <span className="text-xs bg-primary-600 text-white px-4 py-1.5 mr-3"><AiFillStar className='w-4 h-4 text-yellow-400' /></span> <span className="text-sm font-medium font-['owners']">Tickets on sale 5/14</span>
+                        <span className="text-xs bg-primary-600 text-white px-4 py-1.5 mr-3"><AiFillStar className='w-4 h-4 text-yellow-400' /></span> <span className="text-sm font-medium font-['owners2']">Tickets on sale 5/14</span>
                         <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                     </Link>
                     <img className='max-w-xs p-12 mx-auto hover:scale-110 duration-150 ease-in-out -z-0 relative' src="/images/logo_trans.png"></img>
