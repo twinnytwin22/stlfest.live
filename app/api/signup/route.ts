b9ede24b8318eaf4ed0b554eb9e58d5546bba2ff
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-11 22:02:01 -0700

    testing signups

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,16 +1,32 @@
 import { google } from "googleapis";
 
-const sheets = google.sheets({ version: "v4" });
-const spreadsheetId = process.env.SHEET_ID;
-const range = "Sheet1!A1";
+type SignUpEmail = {
+  email: string;
+};
 
 export async function POST(req: Request) {
-  const { email } = await req.json();
+  const { email } = (await req.json()) as SignUpEmail;
   if (!email) {
     return new Response("error: Email is required");
   }
   try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      },
+      scopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive/spreedsheets",
+      ],
+    });
+
+    const sheets = google.sheets({ version: "v4", auth });
+    const spreadsheetId = process.env.SHEET_ID;
+    const range = "Sheet1!A1:B1";
     console.log("getting result");
+
     const result = await sheets.spreadsheets.values.append({
       spreadsheetId,
       range: range, // Append to column A of Sheet1
---
+++ lib/googleApi.tsx
@@ -0,0 +1,66 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import process from 'process';
+import { authenticate } from '@google-cloud/local-auth';
+import { google, sheets_v4 } from 'googleapis';
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content.toString());
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+async function saveCredentials(client: any) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content.toString());
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  }) as any
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+export async function listMajors(auth: any) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E',
+  }) as sheets_v4.Schema$ValueRange
+  const rows = res?.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  rows.forEach((row) => {
+    console.log(`${row[0]}, ${row[4]}`);
+  });
+}
+
+authorize().then(listMajors).catch(console.error);
