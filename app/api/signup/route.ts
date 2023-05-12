34a74af04861b864ce99464e4b8d546b7e4726ed
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-11 19:07:10 -0700

    Update route.ts

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,41 +1,29 @@
-import sgMail from "@sendgrid/mail";
-import { NextResponse } from "next/server";
 import { google } from "googleapis";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 const sheets = google.sheets({ version: "v4" });
 const spreadsheetId = process.env.SHEET_ID;
-const range = "Sheet1!A1:E1";
+const range = "Sheet1!A1";
 
 export async function POST(req: Request) {
   const { email } = await req.json();
   if (!email) {
     return new Response("error: Email is required");
   }
-
-  const msg = {
-    to: email,
-    cc: process.env.FROM_EMAIL as string,
-    from: process.env.FROM_EMAIL as string,
-    html: ``,
-  };
-
   try {
-    await sgMail.send(msg);
-    console.log("Email sent");
-    const newRow = [email, new Date().toLocaleString()];
-    await sheets.spreadsheets.values.append({
+    console.log("getting result");
+    const result = await sheets.spreadsheets.values.append({
       spreadsheetId,
-      range,
+      range: range, // Append to column A of Sheet1
       valueInputOption: "USER_ENTERED",
       requestBody: {
-        values: [newRow],
+        values: [[email]],
       },
     });
-
-    return new Response("success: true");
+    console.log(
+      `Appended "${email}" to Sheet1!A${result.data.updates.updatedRange}`
+    );
   } catch (error) {
-    console.error(error);
-    new Response("error: Error sending email");
+    console.error(`Error appending "${email}" to Sheet1!A:A:`, error);
+    return new Response("error: Error appending email to sheet");
   }
 }
