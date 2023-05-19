1291c90a052315da1d69d1ca4795d466e297e091
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-19 09:40:06 -0700

    flashsale ready

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import FlashSale from '../../ui/FlashSale'
 
@@ -5,6 +6,13 @@ function page() {
     return (
         <div className='w-screen justify-center content-center items-center mx-auto p-16 bg-white h-full min-h-screen overflow-hidden will-change-auto'>
             <FlashSale />
+            <div className='mx-auto'>
+                <Link href="https://www.ticketfalcon.com/e/stlfestflashsale/">
+                    <div className='bg-blue-600 p-2.5 max-w-sm flex w-full mx-auto justify-center md:hover:scale-105 hover:bg-blue-800'>
+                        <h2 className=' text-lg text-center font-[owners2] text-white'>Having trouble? Checkout straight from Ticket Falcon</h2>
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 }
