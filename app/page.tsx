ba68b09728d4195667d9c27acb573a20c85971fa
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-19 08:22:40 -0700

    flash prep

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import PhotoSlide from '../ui/PhotoSlide'
 import HomeHeader from '../ui/HomeHeader'
 import VendorVolunteer from '../ui/VendorVolunteer'
+import FlashSale from '../ui/FlashSale'
 
 function page() {
     return (
         <div className='mt-16'>
             <HomeHeader />
+            <FlashSale/>
             <PhotoSlide />
             <VendorVolunteer />
         </div>
---
+++ ui/FlashSale.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Iframe from "react-iframe";
+
+function FlashSale() {
+    return (
+        <Iframe
+            width="100%"
+            height="600"
+            className="mx-auto bg-white p-4 overflow-auto touch-auto"
+            url="https://www.ticketfalcon.com/embed-tickets/1627153/1"
+        ></Iframe >
+    );
+}
+
+export default FlashSale;
