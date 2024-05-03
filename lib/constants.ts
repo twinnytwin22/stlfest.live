7209307e7d31c2d75dcba2909319c5c4444815ec
Twinny Twin <154267258+HerndonAZ@users.noreply.github.com>
2024-05-03 10:00:42 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
-export const volunteerAppLink = 'https://docs.google.com/forms/d/1ho_d7KaYBGD8oOOmk5Iy6Gn7CEaBwR5LHcBmFzhxxP8'
+export const volunteerAppLink = 'https://docs.google.com/forms/d/e/1FAIpQLSf9WOkvUkUmhfMHnQfsjyk_c7JmeeotjbxIeHa8EVnuQHlW9A/viewform'
 
-export const venderAppLink = 'https://docs.google.com/forms/d/1bJeKskdAwyYpTyF71e2ByBERRb_dKCSMVdrAkZ0hb1A'
+export const venderAppLink = 'https://docs.google.com/forms/d/e/1FAIpQLScck9ejq9kbYWch_9DueTIWPR71GyyjGnGxgWsWEMX0nxPsIA/viewform'
 export const peoplesChoice = 'https://docs.google.com/forms/d/e/1FAIpQLSf6PVuLMC0F86awl4fMiysN2hrqNcMJQuJjtXjNhWbnI3Ligw/viewform'
 export const flashSaleLink = 'https://www.ticketfalcon.com/e/stlfest24/'
 
